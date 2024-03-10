58603720270083e872fb41ae5605be6aa07d3291
Robert Tyssen <rob.tyssen@gmail.com>
2024-03-10 12:00:50 -0400

    Added 'more' button to ChatHeader

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,10 +1,17 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { MoreVertRounded } from '@mui/icons-material';
+import { Avatar, Box, IconButton, Typography } from '@mui/material';
+import React from 'react';
 
 const ChatHeader = () => {
+
+  const chatTitle = 'Chat Title';
+
   return (
-    <Box sx={{boxSizing: 'border-box', p: 4}}>
-      <Typography>Chat Header</Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center', p: 4 }}>
+      <Avatar sx={{ marginRight: 2 }} />
+      <Typography variant='h5'>{chatTitle}</Typography>
+
+      <IconButton sx={{marginLeft: 'auto'}}><MoreVertRounded /></IconButton>
     </Box>
   )
 }
